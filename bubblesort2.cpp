1eaa2d7bb4bb47240ab66d3ca12579795b4ef152
Archita Das <darchita06@gmail.com>
2024-08-28 20:58:36 +0530

    added a checking condition to check whether the input array is already sorted or not

--- bubblesort2.cpp
+++ bubblesort2.cpp
@@ -9,13 +9,16 @@ int main(){
 
     }
     for(int i=n-1;i>=1;i--){
+        bool b=false;
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
                 int t=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=t;
+                b=true;
             }
         }
+        if(!b)break;
     }
     for(int i=0;i<n;i++){
         cout<<arr[i]<<' ';
