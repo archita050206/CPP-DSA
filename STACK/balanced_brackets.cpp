57c7fb74af272696ebbe1e345abe72904f813b85
Archita Das <darchita06@gmail.com>
2024-09-26 20:27:35 +0530

    check for balanced brackets

---
+++ STACK/balanced_brackets.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool  isBalanced(string str){
+    stack<char>st;
+    bool flag=true;
+    for(char c:str){
+        if(c=='('||c=='{'||c=='['){
+            st.push(c);
+        }
+        else if(c==']'){
+            if(!st.empty() && st.top()=='[')st.pop();
+            else{
+                flag=false;
+                break;
+            }
+        }
+        else if(c=='}'){
+            if(!st.empty() && st.top()=='{')st.pop();
+            else{
+                flag=false;
+                break;
+            }
+        }
+        else if(c==')'){
+            if(!st.empty() && st.top()=='(')st.pop();
+            else{
+                flag=false;
+                break;
+            }
+        }
+    }
+    if(!st.empty())flag=false;
+    return flag;
+}
+int main(){
+    string str="{[[()]]}";
+    bool b=isBalanced(str);
+    cout<<b;
+}
