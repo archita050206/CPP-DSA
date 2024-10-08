9ad32342421a8a46e8786e17f40271985b1cabe4
Archita Das <darchita06@gmail.com>
2024-10-08 19:55:30 +0530

    zombie game

--- OOPs/ZombieShooter.cpp
+++ OOPs/ZombieShooter.cpp
@@ -3,18 +3,31 @@
 using namespace std;
 class MasterZombie{
     public:
-    string name;
-    int heaight;
-    int power;
+    string name="Zaza";
+    int height=50;
+    int power=40;
+
+    virtual void changeHealth(){
+        power-=20;
+    }
 };
 class zombie1 : public MasterZombie{
     string description;
     void fly(){
-
+        cout<<name<<endl;
     }
 };
 class zombie2: public MasterZombie{
+    public:
     void spitFire(){
 
     }
+    void changeHealth() override{
+        cout<<"Health";
+    }
 };
+int main(){
+    zombie1 z;
+    MasterZombie *m1=new zombie2();
+    m1->changeHealth();
+}
