477d0c2f1198bb60d3553707697db0025c5bfa0c
Archita Das <darchita06@gmail.com>
2024-10-13 16:14:52 +0530

    lcm of three numbers

---
+++ recursion/lcm.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b){
+    if(b==0)return a;
+
+}
+int lcm(int a, int b){
+    return ((a*b)/gcd(a,b));
+}
+int finalLCM(int a, int b, int c){
+    return lcm(lcm(a,b),c);
+}
+int main(){
+    int x=finalLCM(5,10,15);
+    cout<<x;
+}
