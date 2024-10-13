771ed2f6fee4006c983f03c227c7fc0ce68315bc
Archita Das <darchita06@gmail.com>
2024-10-13 16:11:42 +0530

    tower of hanoi problem for 2 disks

--- recursion/tower_of_hanoi.cpp
+++ recursion/tower_of_hanoi.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
-void tower(int n, char from, char to, char aux){
+void tower(int n, char from, char to){
     if(n==0){
         return;
     }
-    tower(n-1,from,aux,to);
+    tower(n-1,from,to);
     cout<<"Move from "<<from<<" to "<<to<<endl;
-    tower(n-1,aux,to,from);
+    tower(n-1,to,from);
 }
 int main(){
-    int n=3;
-    tower(3,'A','B','C');
+    int n=2;
+    tower(2,'A','B');
     
 }
