2acca679083bfc313646c8aade1707f6bf85e69c
Archita Das <darchita06@gmail.com>
2024-10-22 20:54:29 +0530

    insert and in order  printing

---
+++ TREE/BST_insert.cpp
@@ -0,0 +1,43 @@
+//Inorder traversal in BST is in sorted order: ascending order
+
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *right;
+    Node *left;
+    Node(int val){
+        data=val;
+        left=right=nullptr;
+    }
+};
+Node *insert(Node *root, int val){
+    if(root==nullptr){
+        return new Node(val);
+    }
+    if(val<root->data){
+        root->left=insert(root->left,val);
+
+    }
+    else{
+        root->right=insert(root->right,val);
+    }
+    return root;
+}
+void inOrder(Node *root){
+    if(!root)return;
+    inOrder(root->left);
+    cout<<root->data<<' ';
+    inOrder(root->right);
+}
+int main(){
+    Node *root=NULL;
+    root=insert(root,5);
+    insert(root,1);
+    insert(root,3);
+    insert(root,4);
+    insert(root,2);
+    insert(root,7);
+    inOrder(root);
+}
