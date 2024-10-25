b97612305daeacfeb55f7a5e4f6dd548530a9b37
Archita Das <darchita06@gmail.com>
2024-10-25 18:54:17 +0530

    tower of hanoi for 3 disks

--- recursion/tower_of_hanoi.cpp
+++ recursion/tower_of_hanoi.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 using namespace std;
-void tower(int n, char from, char to){
+int s=0;
+int tower(int n, char from, char to, char aux){
     if(n==0){
-        return;
+       // s++;
+        return s;
     }
-    tower(n-1,from,to);
+    tower(n-1,from,aux,to);
+    s++;
     cout<<"Move from "<<from<<" to "<<to<<endl;
-    tower(n-1,to,from);
+    tower(n-1,aux,to,from);
 }
 int main(){
-    int n=2;
-    tower(2,'A','B');
-    
+    int n=3;
+    int sum=tower(3,'A','B','C');
+    cout<<sum<<endl;
 }
