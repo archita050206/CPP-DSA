a5c7a3a7b680e1331e1ef7a08750985d3a9ac824
Archita Das <darchita06@gmail.com>
2024-11-21 20:59:41 +0530

    compression and its solution

---
+++ HASHING/collision.cpp
@@ -0,0 +1,12 @@
+// hash function: converts the input code into some int or val
+//                 called hash code
+// compression: converting the number into some value which can ebe stored in the
+//                 finite range of the hash table that i created
+
+//collision: when there is more than 1 value of key for the same index
+
+// solution: 
+// 1. open hashing(seperate chaining): use linked list, the head of the linked list is 
+//     stored in the index
+//
+// 2. close hashing (probing): find the other element some other place in the hash table 
--- HASHING/intro.cpp
+++ HASHING/intro.cpp
@@ -21,3 +21,4 @@ int main()
         cout << itr->first << " " << itr->second << endl;
     }
 }
+
