1fb52d0750fcdec3cb7dce236bcca74c46bc4f22
Archita Das <darchita06@gmail.com>
2024-11-25 20:09:56 +0530

    steps for kahn's algorithm

---
+++ GRAPH/kahn's_algo.cpp
@@ -0,0 +1,8 @@
+// find indegree of a;; the nodes
+//push to the queue is indegree of anynode is 0
+
+
+//STEPS:
+//add the element with indegree 0
+//pop it from the stack and update the indegree o f the neighbouring nodes
+//add the node to a vector (answer) 
